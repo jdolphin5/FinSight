a3b110335830e10e6cda5162f027d73960665f75
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-09-16 13:21:17 +1000

    addedtests for multiple read/update, delete

--- server/finsight/src/test/java/com/jd/finsight/repositories/StockRepositoryIntegrationTests.java
+++ server/finsight/src/test/java/com/jd/finsight/repositories/StockRepositoryIntegrationTests.java
@@ -4,9 +4,11 @@ import com.jd.finsight.TestDataUtil;
 import com.jd.finsight.domain.Stock;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
@@ -14,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 // included with @SpringBootTest
-// @ExtendWith(SpringApplication.class)
+@ExtendWith(SpringExtension.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class StockRepositoryIntegrationTests {
     private StockRepository underTest;
@@ -32,5 +34,44 @@ public class StockRepositoryIntegrationTests {
         Optional<Stock> result = underTest.findById(stock.getId());
         assertThat(result).isPresent();
         assertThat(result.get()).isEqualTo(stock);
+    }
+
+    @Test
+    public void testThatMultipleStocksCanBeCreatedAndRecalled() {
+        Stock stockA = TestDataUtil.createTestStockA();
+        underTest.save(stockA);
+
+        Stock stockB = TestDataUtil.createTestStockB();
+        underTest.save(stockB);
+
+        Stock stockC = TestDataUtil.createTestStockC();
+        underTest.save(stockC);
+
+        Iterable<Stock> result = underTest.findAll();
+        assertThat(result)
+                .hasSize(3)
+                .containsExactly(stockA, stockB, stockC);
+    }
+
+    @Test
+    public void testThatStockCanBeUpdated() {
+        Stock stockA = TestDataUtil.createTestStockA();
+        underTest.save(stockA);
+
+        stockA.setCode("NVDA");
+        underTest.save(stockA);
+
+        Optional<Stock> result = underTest.findById(stockA.getId());
+        assertThat(result).isPresent();
+        assertThat(result.get()).isEqualTo(stockA);
+    }
+
+    @Test
+    public void testThatStockCanBeDeleted() {
+        Stock stockA = TestDataUtil.createTestStockA();
+        underTest.save(stockA);
+        underTest.deleteById(stockA.getId());
+        Optional<Stock> result = underTest.findById(stockA.getId());
+        assertThat(result).isEmpty();
     }
 }
