aef152f632ed6582c1b7a7e216320896d29e9cdf
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-09-16 13:16:23 +1000

    added multiple create Stock methods

--- server/finsight/src/test/java/com/jd/finsight/TestDataUtil.java
+++ server/finsight/src/test/java/com/jd/finsight/TestDataUtil.java
@@ -7,7 +7,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 
-public class TestDataUtil {
+public final class TestDataUtil {
     public static Stock createTestStockA() {
         return Stock.builder()
                 .id(1L)
@@ -17,6 +17,32 @@ public class TestDataUtil {
                 .low(228.957)
                 .high(228.957)
                 .close(228.957)
+                .volume(1.0)
+                .build();
+    }
+
+    public static Stock createTestStockB() {
+        return Stock.builder()
+                .id(2L)
+                .code("AAPL")
+                .local_time(Timestamp.valueOf(convertRawTimestamp("03.09.2024 00:00:00.000")))
+                .open(222.5)
+                .low(222.5)
+                .high(222.5)
+                .close(222.5)
+                .volume(1.0)
+                .build();
+    }
+
+    public static Stock createTestStockC() {
+        return Stock.builder()
+                .id(3L)
+                .code("DDOG")
+                .local_time(Timestamp.valueOf(convertRawTimestamp("04.09.2024 00:00:00.000")))
+                .open(109.26)
+                .low(109.26)
+                .high(109.26)
+                .close(109.26)
                 .volume(1.0)
                 .build();
     }
