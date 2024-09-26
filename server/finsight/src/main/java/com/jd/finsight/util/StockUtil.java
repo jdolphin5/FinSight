4a3cb61102c0f19209d2a031e4fade61b2d0c02a
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-09-26 14:53:20 +1000

    static method to filter list by stock code

--- server/finsight/src/main/java/com/jd/finsight/util/StockUtil.java
+++ server/finsight/src/main/java/com/jd/finsight/util/StockUtil.java
@@ -3,6 +3,10 @@ package com.jd.finsight.util;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.jd.finsight.domain.HistoricalStockDataEntity;
 
 public class StockUtil {
     public static LocalDateTime convertRawTimestamp(String s) {
@@ -16,5 +20,19 @@ public class StockUtil {
         }
 
         return localDateTime;
+    }
+
+    public static List<HistoricalStockDataEntity> filterByCode(
+            List<HistoricalStockDataEntity> historicalStockDataEntityList,
+            String code) {
+        List<HistoricalStockDataEntity> historicalStockDataEntityWithCodeList = new ArrayList<>();
+
+        for (HistoricalStockDataEntity e : historicalStockDataEntityList) {
+            if (e.getCode().equals(code)) {
+                historicalStockDataEntityWithCodeList.add(e);
+            }
+        }
+
+        return historicalStockDataEntityWithCodeList;
     }
 }
