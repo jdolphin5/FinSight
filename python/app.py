07ac038879e322fad6131f632651bea6319fe1ac
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-10-08 15:43:09 +1100

    cleanup graph & separate into folders

--- python/app.py
+++ python/app.py
@@ -1,11 +1,9 @@
 from flask import Flask, jsonify
 from dash import Dash, html, dcc, Input, Output
-import plotly.graph_objects as go
 import requests
 import pandas as pd  # Import pandas for data manipulation
-from math_calcs import calculate_wma, calculate_macd, calculate_true_range, calculate_dm, calculate_di
-from math_calcs import calculate_adx, calculate_parabolic_sar, calculate_rsi, calculate_stochastic_oscillator
-from math_calcs import calculate_cci, calculate_bollinger_bands, calculate_atr
+from calculations.math_calcs import calculate_wma
+from visualisation.figures import generate_figures
 
 # Initialize Flask server
 server = Flask(__name__)
@@ -24,13 +22,13 @@ colours = {
 
 # Example stock codes for dropdown
 available_stocks = ['AAPL', 'AMZN', 'GOOGL', 'TSLA', 'MSFT']
-available_graph_types = ['Standard', 'MACD - Moving Average Convergence/Divergence', 'ADX - Average Directional Index', 'Parabolic SAR - Stop and Reverse',
+available_graph_types = ['Standard / OHLC', 'EMA - Exponential Moving Average', 'MACD - Moving Average Convergence/Divergence', 'ADX - Average Directional Index', 'Parabolic SAR - Stop and Reverse',
                          'RSI - Relative Strength Index', '%K - Stochastic Oscillator', 'CCI - Commodity Channel Index',
                          'Bollinger Bands', 'ATR - Average True Range']
 
 # Define the Dash layout
 app.layout = html.Div([
-    html.H1("Stock Data Visualization"),
+    html.H1("Stock Data Visualisation"),
 
     # Dropdown to select stock code
     dcc.Dropdown(
@@ -43,7 +41,7 @@ app.layout = html.Div([
     dcc.Dropdown(
         id='graph-type-dropdown',
         options=[{'label': graph_type, 'value': graph_type} for graph_type in available_graph_types],
-        value='Standard',
+        value='Standard / OHLC',
         placeholder='Select Graph Type'
     ),
 
@@ -119,7 +117,7 @@ def update_graph(selected_stock, selected_graph_type, n_clicks_5y, n_clicks_1y, 
     
     df_filtered = df
     df_filtered = df_filtered.copy()
-    
+
     if relayoutData and 'xaxis.range[0]' in relayoutData and 'xaxis.range[1]' in relayoutData:
         # Extract the uniform x-range (indices) from relayoutData
         start_idx = int(float(relayoutData['xaxis.range[0]']))  # relayoutData returns strings, so cast to float first
@@ -142,380 +140,7 @@ def update_graph(selected_stock, selected_graph_type, n_clicks_5y, n_clicks_1y, 
     stock_times = df_filtered['local_time']  # Using the DataFrame column
     stock_close_prices = df_filtered['close']  # Using the DataFrame column
 
-    uniform_x = list(range(len(df_filtered)))
-
-    num_ticks = 3 if len(df_filtered) < 4 else 4  # Show 3 or 4 ticks depending on data size
-    step = max(1, len(df_filtered) // num_ticks)  # Ensure dynamic spacing based on visible data
-    reduced_x = uniform_x[::step]  # Uniform x values reduced
-    reduced_dates = stock_times.dt.strftime('%d-%m-%Y')[::step]
-
-    df_filtered['EMA'] = df_filtered['close'].ewm(span=10, adjust=False).mean()
-    stock_ema = df_filtered['EMA']
-
-    asterix_info = ''
-
-    figure = {
-        'data': [
-            go.Scatter(
-                x=uniform_x,
-                y=stock_close_prices,
-                mode='lines',
-                name=f'{selected_stock}',
-                marker=dict(size=3),
-                text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                hoverinfo='text+y'
-            ),
-            go.Scatter(
-                x=uniform_x,
-                y=stock_ema,
-                mode='lines',
-                name=f'{selected_stock} EMA (10-day)',
-                line=dict(color='orange'),
-                marker=dict(size=2),
-                text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                hoverinfo='text+y'
-            )
-        ],
-        'layout': go.Layout(
-            title=f"Stock: {selected_stock} ({time_range})",
-            xaxis_title="Date",
-            xaxis=dict(
-                tickmode='array',
-                tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-            ),
-            yaxis={'title': 'Price', 'showgrid': False},
-        )
-    }
-
-    # Create the figure for the graph
-    if (selected_graph_type == 'Standard'):
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=stock_close_prices,
-                    mode='lines',
-                    name=f'{selected_stock}',
-                    marker=dict(size=3),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                ),
-                go.Scatter(
-                    x=uniform_x,
-                    y=stock_ema,
-                    mode='lines',
-                    name=f'{selected_stock} EMA (10-day)',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'Price', 'showgrid': False},
-            )
-        }
-    elif (selected_graph_type == 'MACD - Moving Average Convergence/Divergence'):
-        df_macd = calculate_macd(df_filtered)
-
-        asterix_info = 'Moving Average Convergence/Divergence shows the relationship between two EMAs.'
-
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_macd['MACD'],
-                    mode='lines',
-                    name=f'{selected_stock} MACD',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                ),
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_macd['Signal_Line'],
-                    mode='lines',
-                    name='Signal Line',
-                    line=dict(color='green'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'Price', 'showgrid': False},
-            )
-        }
-    elif (selected_graph_type == 'ADX - Average Directional Index'):        
-        df_filtered = calculate_true_range(df_filtered)
-        df_filtered = calculate_dm(df_filtered)
-        df_filtered = calculate_di(df_filtered, period=14)
-        df_filtered = calculate_adx(df_filtered, period=14)
-
-        asterix_info = "Average Directional Index measures the strength of a trend regardless of its direction"
-
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['ADX'],
-                    mode='lines',
-                    name=f'{selected_stock} ADX (period: 14)',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'Price', 'showgrid': False},
-            )
-        }
-    elif (selected_graph_type == 'Parabolic SAR - Stop and Reverse'):
-        if (len(df_filtered) == 0):
-            asterix_info = 'There is no data in this range'
-
-        else:
-            df_filtered = calculate_parabolic_sar(df_filtered)
-
-            asterix_info = "Parabolic Stop and Reverse is a time/price-based indicator used to identify potential reverse points. *AF = Acceleration Factor, initial_af=0.02, max_af=0.20, step_af=0.02"
-
-            figure = {
-                'data': [
-                    go.Scatter(
-                        x=uniform_x,
-                        y=df_filtered['SAR'],
-                        mode='lines',
-                        name=f'{selected_stock} SAR',
-                        line=dict(color='orange'),
-                        marker=dict(size=4),
-                        text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                        hoverinfo='text+y'
-                    ),
-                                    go.Scatter(
-                        x=uniform_x,
-                        y=df_filtered['high'],
-                        mode='lines',
-                        name=f'{selected_stock} High',
-                        line=dict(color='red'),
-                        marker=dict(size=2),
-                        text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                        hoverinfo='text+y'
-                    ),
-                    go.Scatter(
-                        x=uniform_x,
-                        y=df_filtered['low'],
-                        mode='lines',
-                        name=f'{selected_stock} Low',
-                        line=dict(color='blue'),
-                        marker=dict(size=2),
-                        text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                        hoverinfo='text+y'
-                    )
-                ],
-                'layout': go.Layout(
-                    title=f"Stock: {selected_stock} ({time_range})",
-                    xaxis_title="Date",
-                    xaxis=dict(
-                        tickmode='array',
-                        tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                        ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                    ),
-                    yaxis={'title': 'Price', 'showgrid': False},
-                )
-            }
-    elif (selected_graph_type == 'RSI - Relative Strength Index'):
-        df_filtered = calculate_rsi(df_filtered)
-
-        asterix_info = 'Relative Strength Index measures the speed and change of price movements.'
-
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['RSI'],
-                    mode='lines',
-                    name=f'{selected_stock} RSI (period: 14)',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'RSI', 'showgrid': False},
-            )
-        }
-
-    elif (selected_graph_type == '%K - Stochastic Oscillator'):  
-        df_filtered = calculate_stochastic_oscillator(df_filtered)
-
-        asterix_info = 'Stochastic Oscillator compares a particular closing price to a range of prices over a certain period. Period=14'
-
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['stochastic_k'],
-                    mode='lines',
-                    name=f'{selected_stock} Stochastic Oscillator (period: 14)',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'Stochastic K', 'showgrid': False},
-            )
-        }
-
-    elif (selected_graph_type == 'CCI - Commodity Channel Index'):  
-        df_filtered = calculate_cci(df_filtered)
-
-        asterix_info = 'Commodity Channel Index measures the variation of a security\'s price from it\'s statistical mean.'
-
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['CCI'],
-                    mode='lines',
-                    name=f'{selected_stock} Commodity Channel Index (period: 20)',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'CCI', 'showgrid': False},
-            )
-        }
-
-    elif (selected_graph_type == 'Bollinger Bands'):  
-        df_filtered = calculate_bollinger_bands(df_filtered)
-
-        asterix_info = 'Bollinger Bands consist of a middle SMA band and two outer bands based on standard deviations.'
-
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['Upper Band'],
-                    mode='lines',
-                    name=f'{selected_stock} Upper Band (period: 20)',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                ),
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['Middle Band'],
-                    mode='lines',
-                    name=f'{selected_stock} Middle Band (period: 20)',
-                    line=dict(color='green'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                ),
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['Lower Band'],
-                    mode='lines',
-                    name=f'{selected_stock} Lower Band (period: 20)',
-                    line=dict(color='blue'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'Bollinger Bands', 'showgrid': False},
-            )
-        }
-
-    elif (selected_graph_type == 'ATR - Average True Range'):  
-        df_filtered = calculate_atr(df_filtered)
-
-        asterix_info = 'Measures market volatility by decomposing the entire range of an asset price for that period.'
-
-        figure = {
-            'data': [
-                go.Scatter(
-                    x=uniform_x,
-                    y=df_filtered['ATR'],
-                    mode='lines',
-                    name=f'{selected_stock} Average True Range (period: 14)',
-                    line=dict(color='orange'),
-                    marker=dict(size=2),
-                    text=stock_times.dt.strftime('%d-%m-%Y, %r'),
-                    hoverinfo='text+y'
-                )
-            ],
-            'layout': go.Layout(
-                title=f"Stock: {selected_stock} ({time_range})",
-                xaxis_title="Date",
-                xaxis=dict(
-                    tickmode='array',
-                    tickvals=reduced_x,  # Positions of ticks are reduced uniform x-values
-                    ticktext=reduced_dates  # Show reduced set of actual dates at each tick
-                ),
-                yaxis={'title': 'ATR', 'showgrid': False},
-            )
-        }
+    asterix_info, figure = generate_figures(df_filtered, selected_graph_type, selected_stock, stock_times, stock_close_prices, time_range)
 
     # Stock information (showing the last close price and time)
     last_entry = df_filtered.iloc[-1] if not df_filtered.empty else {}
