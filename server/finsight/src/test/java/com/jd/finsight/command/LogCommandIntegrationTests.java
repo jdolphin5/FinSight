f3d1d0d6d22884fee45c6a46d5ed4f33ab752791
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-10-12 16:32:23 +1100

    implemented test for log command - log file created when invoked/executed

--- server/finsight/src/test/java/com/jd/finsight/command/LogCommandIntegrationTests.java
+++ server/finsight/src/test/java/com/jd/finsight/command/LogCommandIntegrationTests.java
@@ -1,10 +1,17 @@
 package com.jd.finsight.command;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.jd.finsight.logging.impl.CommandLogGenerator;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -20,8 +27,27 @@ public class LogCommandIntegrationTests {
     // invoker
     private CommandOperationInvoker commandOperationInvoker;
 
-    @Autowired
     public LogCommandIntegrationTests() {
+        this.commandHandler = new CommandHandler();
+        this.logCommand = new LogCommand("test log", commandHandler, new CommandLogGenerator("test.txt"));
+        this.commandOperationInvoker = new CommandOperationInvoker();
+    }
+
+    @AfterEach
+    public void cleanUp() {
+        // Clean up the log file after each test
+        File logFile = new File("test.txt");
+        if (logFile.exists()) {
+            logFile.delete(); // Deletes the file created during the test
+        }
+    }
+
+    @Test
+    public void testThatLogCommandCreatesLogFileWhenLogCommandIsInvoked() {
+        commandOperationInvoker.invokeCommand(logCommand);
+
+        File logFile = new File("test.txt");
+        assertTrue(logFile.exists(), "Log file should be created");
     }
 
 }
